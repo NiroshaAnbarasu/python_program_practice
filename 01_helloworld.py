13b784a1f5f324969e37d99dbc4c5d179cf1a420
NiroshaAnbarasu <anbushanthi96@gmail.com>
2025-08-12 14:20:36 +0530

    Update 01_helloworld.py
    Sample change to check how it is working

--- 01_helloworld.py
+++ 01_helloworld.py
@@ -1,5 +1,5 @@
 # Write a program that asks for the user's name and prints "Hello, [name]!"
 
-print("What's your sweet name")
+print("What's your sweet name: ")
 name = input()
 print("Hello, " + name + "!")
